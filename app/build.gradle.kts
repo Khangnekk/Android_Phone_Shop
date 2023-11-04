6b8042c334023ec90bb8ad5e81f2bdf999d1d547
CoKyAi04 <minh04112002@gmail.com>
2023-11-04 14:19:08 +0700

    Chat Screen Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.fptugroup6.phoneshop"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.fptugroup6.phoneshop"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
