3f6a33f496479f6e4ee2657a7e59378311453f1f
Khangnekk <contact.khalgfk@gmail.com>
2023-11-04 22:28:46 +0700

    add JDBC libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.microsoft.sqlserver:mssql-jdbc:9.4.0.jre8")
 }
