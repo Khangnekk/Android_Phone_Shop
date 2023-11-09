57090640613e00377b3ffd60a139ccdcb394852f
Khangnekk <contact.khalgfk@gmail.com>
2023-11-09 13:46:37 +0700

    Update ApiClient.java

--- app/src/main/java/com/fptugroup6/phoneshop/api/ApiClient.java
+++ app/src/main/java/com/fptugroup6/phoneshop/api/ApiClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static  final  String BASE_URL = "http://192.168.34.108:7017/";
+    private static  final  String BASE_URL = "http://10.33.33.67:7017/";
     private static Retrofit retrofit = null;
     public  static Retrofit getClient(){
         Gson gson = new GsonBuilder()
